954c2f74eddd5ca77bde75cfd931f8d3c19d3dfb
Alan Chen <alanchen@nyu.edu>
2015-10-17 00:42:07 -0400

    update logo

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -6,9 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/stats')
+def stats():
+    return render_template('stats.html')
+
 @app.route('/campaign/<int:cid>')
 def campaign(cid):
     return render_template('campaign.html')
+
 
 if __name__ == '__main__':
     app.run()
