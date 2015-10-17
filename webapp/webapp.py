ed281791301d8c51270434cec2ed50b628a5491d
Alan Chen <alanchen@nyu.edu>
2015-10-16 23:30:30 -0400

    campaign page

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
+@app.route('/campaign/<int:cid>')
+def campaign(cid):
+    return render_template('campaign.html')
 
 if __name__ == '__main__':
     app.run()
